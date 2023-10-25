030030ec0a7e86874d03ca50e5524160ac64765a
Fenus <eboiko1111@gmail.com>
2023-10-26 01:47:21 +0200

    created class controledtable2

--- table.js
+++ table.js
@@ -4,9 +4,10 @@ class ControlTable {
   constructor(
     amountOfRows = 2,
     amountOfColumn = 2,
-    tablePlace = document.getElementById("controlTable")
+    tablePlace = "controlTable"
   ) {
-    this.tablePlace = tablePlace;
+    this.tablePlace = document.getElementById(tablePlace);
+    this.tableName = tablePlace;
     this.amountOfRows = amountOfRows;
     this.amountOfColumn = amountOfColumn;
     this.renderedTableData = [];
@@ -15,18 +16,18 @@ class ControlTable {
   // инкапсулированный класс для создания состояния клетки (будет раширяться для подгрузок и проверок)
 
   #Cell = class {
-    constructor(rowId, colId) {
+    constructor(rowId, colId, tableName) {
       this.rowId = rowId;
       this.colId = colId;
-      this.cellId = `${this.rowId}_${this.colId}`;
+      this.cellId = `${this.rowId}_${this.colId}_${tableName}`;
       this.free = true;
     }
   };
 
   // функция, которая создает экземпляр данных клетки
 
-  #addNewCell(elemRowNumber, elemColNumber) {
-    return new this.#Cell(elemRowNumber, elemColNumber);
+  #addNewCell(elemRowNumber, elemColNumber, tableName) {
+    return new this.#Cell(elemRowNumber, elemColNumber, tableName);
   }
 
   // Вызывается для создания экземпляра таблицы для последущего рендера
@@ -35,7 +36,7 @@ class ControlTable {
     for (let i = 0; i < this.amountOfRows; i++) {
       let row = [];
       for (let j = 0; j < this.amountOfColumn; j++) {
-        row.push(this.#addNewCell(i, j));
+        row.push(this.#addNewCell(i, j, this.tableName));
       }
       this.renderedTableData.push(row);
     }
@@ -76,6 +77,8 @@ class ControlTable {
         .split("_")
         .map(Number);
 
+      console.log(targetPosition);
+
       if (this.#isPlacementPossible(targetPosition, componentData)) {
         this.#mergeCells(event.target, targetPosition, componentData);
       } else {
@@ -89,23 +92,25 @@ class ControlTable {
     targetCell.setAttribute("colspan", componentSize[1]);
     targetCell.setAttribute("main", "true");
 
+    console.log("gdsfsdf");
+
     for (
-      let i = +startCoordinate[0];
-      i < +startCoordinate[0] + +componentSize[0];
+      let i = startCoordinate[0];
+      i < startCoordinate[0] + componentSize[0];
       i++
     ) {
       for (
-        let j = +startCoordinate[1];
-        j < +startCoordinate[1] + +componentSize[1];
+        let j = startCoordinate[1];
+        j < startCoordinate[1] + componentSize[1];
         j++
       ) {
         let chekingElement = document.querySelector(
-          `td[${ControlTable.attribute}="${i}_${j}"]`
+          `td[${ControlTable.attribute}="${this.renderedTableData[i][j].cellId}"]`
         );
 
         if (chekingElement.hasAttribute("main")) {
           chekingElement.style.width = `${
-            (+componentSize[1] / this.renderedTableData[0].length) * 100
+            (componentSize[1] / this.renderedTableData[0].length) * 100
           }%`;
         } else {
           chekingElement.style.display = "none";
@@ -120,6 +125,7 @@ class ControlTable {
       startCoordinate[0] + componentSize[0] > this.renderedTableData.length ||
       startCoordinate[1] + componentSize[1] > this.renderedTableData[0].length
     ) {
+      console.log("not enough space");
       return false;
     }
 
@@ -134,6 +140,7 @@ class ControlTable {
         j++
       ) {
         if (this.renderedTableData[i][j].free === false) {
+          console.log("not free");
           return false;
         }
       }
@@ -147,7 +154,17 @@ class ControlTable {
   }
 }
 
+// TODO сделать список элементы которого можно будет перетаскивать на таблицу для их отображения в них
+// TODO добавить к ним размер компонента и название к которому можно будет привязать компонент вью
+
+class DraggableList {
+  constructor() {}
+}
+
 const controlTable = new ControlTable(5, 7);
 
 controlTable.createNewTable();
 console.log(controlTable.renderedTableData);
+
+const newTable = new ControlTable(3, 3, "newTable");
+newTable.createNewTable();
